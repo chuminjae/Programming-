fe6c0c92e70fefe76ef208d47b3c10b5014f18a3
Lucas Chu <77494039+chuminjae@users.noreply.github.com>
2022-09-20 20:29:22 +0900

    [Bronze V] Title: A+B - 8, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    int a, b;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> a >> b;
+        cout << "Case #" << i + 1 << ": "<< a<<" + " << b << " = " << a + b << "\n";
+    }
+    return 0;
+    }
+
+
+
+
+
