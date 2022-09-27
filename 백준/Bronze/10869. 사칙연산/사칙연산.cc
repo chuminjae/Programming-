38123f7dc47a38aab8ebc1b27de20ea68c1544ac
Lucas Chu <77494039+chuminjae@users.noreply.github.com>
2022-09-27 17:31:06 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n, k;
+    cin >> n >> k;
+    cout << n + k << "\n";
+    cout << n - k << "\n";
+    cout << n * k << "\n";
+    cout << n / k<< "\n";
+    cout <<  n % k << "\n";
+
+
+    }
