b3b99cee710b8df6fca0b90cc04b5d20299c2bf6
Lucas Chu <77494039+chuminjae@users.noreply.github.com>
2022-10-02 22:06:44 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    int k  = 1;
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+      for(int i = 1; i <= k; i++){
+            cout << "*";
+      }
+      k++;
+      cout << "\n";
+
+}
+}
