974bc95cd83b7186bfc83703171f1f0797717f13
Lucas Chu <77494039+chuminjae@users.noreply.github.com>
2022-11-23 22:40:20 +0900

    [Bronze II] Title: Mixing Milk, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/16769. Mixing Milk/Mixing Milk.cc
+++ 백준/Bronze/16769. Mixing Milk/Mixing Milk.cc
@@ -1,27 +1,19 @@
-# include <iostream>
-#include <algorithm>
+# include <bits/stdc++.h>
 using namespace std;
-int c[4];
-int m[4];
-int main() {
-	for (int i = 1; i < 4; i++) {
-		cin >> c[i] >> m[i];
-	}
-	for (int i = 0; i < 100; i++) {
-		int now = (i % 3) + 1;
-		int nxt = (i + 1) % 3 + 1;
-			if (c[nxt] < m[now] + m[nxt]) {
-				m[now] = m[now] + m[nxt] - c[nxt];
-				m[nxt] = c[nxt];
-				
-		}
-			else {
-				m[nxt] = m[now] + m[nxt];
-				m[now] = 0;
-				
-			}
-	}
-	for (int i = 1; i < 4; i++) {
-		cout << m[i] << "\n";
-	}
-}
+int main(){
+    int c[3];
+    int a[3];
+    int amt;
+    for(int i = 0; i < 3; i++){
+        cin >> c[i] >> a[i];
+    }
+    for(int i = 0; i < 100; i++){
+        amt = min(a[i % 3], c[(i + 1) % 3] - a[(i + 1) % 3]);
+        a[i% 3] -= amt;
+        a[(i + 1) % 3] += amt;
+    }
+    for(int i = 0; i < 3; i++){
+        cout << a[i] << "\n";
+    }
+
+    }
