34b984e6b4bcba14b44ee15c7ac1be379174b003
Lucas Chu (Chuminaje) <77494039+chuminjae@users.noreply.github.com>
2023-07-30 17:26:12 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 116 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+struct point{
+    int x,y;
+};
+point arr[100000];
+int cmp(point a,point b)
+{
+    if(a.x==b.x)
+    {
+        return a.y<b.y;
+    }
+    return a.x<b.x;
+}
+int main(){
+   cin >> n;
+   for(int i = 0; i < n; i++){
+    cin >> arr[i].x >> arr[i].y;
+   }
+   sort(arr, arr + n, cmp);
+   for(int i = 0; i < n; i++){
+        cout << arr[i].x <<" " <<arr[i].y << "\n";
+   }
+}
